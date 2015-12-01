57370cf62bfe747f8cf0476990ae6508bb525ea6
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2015-12-01 14:36:02 +0800

    Implement timeout on focus directive

--- directives/bootstrap_directive.js
+++ directives/bootstrap_directive.js
@@ -1,16 +1,16 @@
 "use strict";
 define([], function(){
-	var FocusDirective =  function ($parse) {
+	var FocusDirective =  function ($timeout,$parse) {
 			return{
 				link : function(scope, element,attrs) {
 					scope.$watch($parse(attrs.focus), function(value) {
 						if (value === true)
-							element[0].focus();
+							$timeout(function(){element[0].focus();});
 					});
 				}
 			}
         };
-	FocusDirective.$inject = ['$parse'];
+	FocusDirective.$inject = ['$timeout','$parse'];
 	return {
 		focus: FocusDirective
 	};
