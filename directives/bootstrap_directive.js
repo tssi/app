bb61a23b92312c215281eb5a8f5e668ceb58d6da
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-02-18 13:40:12 +0800

    Modified keypress validation for monetary

--- directives/bootstrap_directive.js
+++ directives/bootstrap_directive.js
@@ -61,6 +61,17 @@ define([], function(){
 				element.on('focus', function () {
 				  element.val(ngModel.$modelValue);
 				});
+				var keyCode = [8, 9, 37, 39, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 109, 110, 189];
+				element.bind("keydown", function(event) {
+					if (keyCode.indexOf(event.which) === -1) {
+						scope.$apply(function() {
+							scope.$eval(attr.monetary);
+							event.preventDefault();
+						});
+						event.preventDefault();
+					}
+
+				});
 			  }
 			};
 		}
