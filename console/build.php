33e69b1aa4e21889b23c6406d5eb3ee5108507f5
Dave Arroyo <arroyo.daveil@gmail.com>
2017-04-23 12:38:42 +0800

    Adjust build script

--- console/build.php
+++ console/build.php
@@ -3,10 +3,26 @@
 	$path = dirname(__FILE__).DS.'config.json';
 	$config = json_decode(file_get_contents($path),true);
 	$path = dirname(dirname(__FILE__)).DS;
+	$cleanStarted = false;
+	$delay =150000;
+	echo "Build app started \n";
+	usleep($delay);
 	foreach($config['files'] as $file){
-		if(file_exists($file))
+		$performClean = false;
+		if(file_exists($file)){
 			unlink($file);
+			$performClean = true;
+			usleep($delay);
+		}
+		if($performClean&&!$cleanStarted){
+			echo "Cleaning files... \n"; 
+			$cleanStarted=true;
+		}
+		if($performClean){
+			echo "\t$file\n";
+		}
 	}
+	
 	function recurseRmdir($dir) {
 	  $files = array_diff(scandir($dir), array('.','..'));
 	  foreach ($files as $file) {
@@ -14,26 +30,26 @@
 	  }
 	  return rmdir($dir);
 	}
+	echo "Initializing directories... \n"; 
+	usleep($delay);
 	foreach($config['folders'] as $folder){
 		if(file_exists($folder))
 			recurseRmdir($folder);
 		mkdir($folder);
-		
+		echo "\t$folder\n";
+		usleep($delay);
 		
 	}
-	
+	echo "Copying files...\n"; 
+	usleep($delay);
 	foreach($config['files'] as $file){
 		if(file_exists($path.$file))
 			copy($path.$file,$file);
+		echo "\t..$file > $file\n";
+		usleep($delay);
 	}
-	foreach($config['findreplace'] as $file=>$action){
-		$content = file_get_contents($file);
-		foreach($action['find'] as $i=>$key){
-			$content = str_replace($key,$action['replace'][$i],$content);
-			file_put_contents($file,$content);
-		}
-		
-	}
+	echo "Sanitizing index.html...\n";
+	usleep($delay);
 	$index = dirname(dirname(__FILE__));
 	$html = file_get_contents($index.DS.'index.html');
 	$dom = new DOMDocument('1.0', 'UTF-8');                         // init new DOMDocument
@@ -42,6 +58,7 @@
 	$dom->loadHTML($html);                           // load HTML into it
 	$dom->formatOutput =true;
 	libxml_use_internal_errors($internalErrors);
+	echo "\tResetting sidebar menu...\n"; 
 	$xpath = new DOMXPath($dom);                     // create a new XPath
 	$nodes = $xpath->query('//ul[@class="sidebar-nav"]/li/ul/li'); // Find all UL with class
 	foreach($nodes as $node) {                       // Iterate over found elements
@@ -54,4 +71,19 @@
 	$ul->appendChild($fragment);
 	$index = dirname($index);
 	file_put_contents($index.DS.'index.html',$dom->saveHTML());   
+	
+	echo "\tUpdating paths...\n"; 
+	usleep($delay);
+	foreach($config['findreplace'] as $file=>$action){
+		$content = file_get_contents($file);
+		echo "\t\tReplacing $file\n";
+		usleep($delay);
+		foreach($action['find'] as $i=>$key){
+			$replacement = $action['replace'][$i];
+			$content = str_replace($key,$replacement,$content);
+			$f = file_put_contents($file,$content);
+			echo "\t\t\t $f $key > $replacement \n";
+		}
+	}
+	echo "Build app finished";
 ?>
