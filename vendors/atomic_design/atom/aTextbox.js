96867cc97fb23c2d434a834e260a38bed5a26372
Dave Alaras <arroyo.daveil@gmail.com>
2020-07-28 06:58:52 +0800

    Add a-textbox[type]

--- vendors/atomic_design/atom/aTextbox.js
+++ vendors/atomic_design/atom/aTextbox.js
@@ -9,13 +9,15 @@ define(['app'], function (app) {
 			scope:{
 				aTextboxModel:'=ngModel',
 				isDisabled:'=?ngDisabled',
-				aPlaceholder:'=?placeholder'
+				aPlaceholder:'=?placeholder',
+				aType:'=?type',
 			},
 			transclude: false,
 			templateUrl:function(elem,attr){
 				return aPath.url('/view/atom/aTextbox.html');
 			},
 			link: function($scope, elem, attrs,ngModel) {
+				$scope.aType =  $scope.aType||DEFAULTS.type;
 				
 			},
 			controller:function($scope){
