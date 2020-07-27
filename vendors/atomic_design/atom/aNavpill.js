b44cb688311e0d8bbd891aa02f939e426dc029a9
Dave Alaras <arroyo.daveil@gmail.com>
2020-07-27 14:27:43 +0800

    Add a-navtab & update a-navpill

--- vendors/atomic_design/atom/aNavpill.js
+++ vendors/atomic_design/atom/aNavpill.js
@@ -1,7 +1,7 @@
 "use strict";
 define(['app'], function (app) {
 	app.register.directive('aNavpill',['AtomicPath',function (aPath) {
-		const DEFAULTS = {prefix:'',type:'text',size:''};
+		const DEFAULTS = {prefix:'',type:'text',size:'',label:'name'};
 
 		return {
 			require:'ngModel',
@@ -9,6 +9,7 @@ define(['app'], function (app) {
 			scope:{
 				ActiveItem:'=ngModel',
 				Options:'=options',
+				label:'=?',
 				confirm:'&'
 			},
 			replace:true,
@@ -17,13 +18,46 @@ define(['app'], function (app) {
 				return aPath.url('/view/atom/aNavpill.html');
 			},
 			link: function($scope,elem, attrs,ngModel) {
-			
+
+				$scope.aNavLabel = $scope.label||DEFAULTS.label;
+
 			},
 			
 			controller:function($scope){
 
 				$scope.setActiveItem = function(item){
-					$scope.ActiveItem = item;
+					$scope.ActiveItem = item.id;
+				}
+			}
+		}
+	}]);
+	app.register.directive('aNavtab',['AtomicPath',function (aPath) {
+		const DEFAULTS = {prefix:'',type:'text',size:'',label:'name'};
+
+		return {
+			require:'ngModel',
+			restrict: 'E',
+			scope:{
+				ActiveItem:'=ngModel',
+				Options:'=options',
+				label:'=?',
+				confirm:'&'
+			},
+			replace:true,
+			transclude:false,
+			templateUrl:function(elem,attr){
+				return aPath.url('/view/atom/aNavtab.html');
+			},
+			link: function($scope,elem, attrs,ngModel) {
+
+				$scope.aNavLabel = $scope.label||DEFAULTS.label;
+
+			},
+			
+			controller:function($scope){
+
+				$scope.setActiveItem = function(item){
+					$scope.ActiveItem = item.id;
 				}
 			}
 		}
