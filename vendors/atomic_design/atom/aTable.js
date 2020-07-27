559d65aeaee6028796f98cc4aba307d7ef227938
Dave Alaras <arroyo.daveil@gmail.com>
2020-07-27 13:49:12 +0800

    Add aTable factory helper

--- vendors/atomic_design/atom/aTable.js
+++ vendors/atomic_design/atom/aTable.js
@@ -1,6 +1,6 @@
 "use strict";
 define(['app'], function (app) {
-	app.register.directive('aTable',['AtomicPath',function (aPath) {
+	app.register.directive('aTable',['AtomicPath','aTable',function (aPath,aTable) {
 		return {
 			require:'ngModel',
 			restrict: 'E',
@@ -61,5 +61,70 @@ define(['app'], function (app) {
 				}
 			}
 		}
+	}]);
+	app.register.factory("aTable",[function(){
+		var aTable = {
+			colHeaders:function(headers,props){
+				var colClass ="";
+				if(typeof headers=="string"){
+					var propsLen = props.length;
+					if(propsLen<5){
+						var size= (12/propsLen)
+						colClass = ' col-md-'+size+' col-sm-'+size;
+					}
+				}
+
+				angular.forEach(headers, function(hdr,i){
+					if(typeof hdr == 'string'){
+						hdr = {label:hdr,class:colClass};
+					}
+					
+					headers[i] = hdr;
+				});
+				return headers;
+			}
+			isEqual:function(a,b){
+					// Create arrays of property names
+				    var aProps = Object.getOwnPropertyNames(a);
+				    var bProps = Object.getOwnPropertyNames(b);
+
+				    // If number of properties is different,
+				    // objects are not equivalent
+				    if (aProps.length != bProps.length) {
+				        return false;
+				    }
+
+				    for (var i = 0; i < aProps.length; i++) {
+				        var propName = aProps[i];
+
+				        // If values of same property are not equal,
+				        // objects are not equivalent
+				        if (a[propName] !== b[propName]) {
+				            return false;
+				        }
+				    }
+
+			    
+			    // If we made it this far, objects
+			    // are considered equivalent
+			    return true;
+			},
+			isArrayEqual:function(A,B){
+				var isEqual = false;
+				for(var i in A){
+					var a =  A[i];
+					var b =  B[i];
+					isEqual = isEqual && aTable.isEqual(a,b);
+				}
+				for(var i in B){
+					var a =  A[i];
+					var b =  B[i];
+					isEqual = isEqual && aTable.isEqual(a,b);
+				}
+				return isEqual;
+			}
+
+		};
+		return aTable;
 	}]);
 });
