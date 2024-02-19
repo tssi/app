9845c12d23fa7a53dac952a2f64830ae2ec4a322
Dave Arroyo <arroyo.daveil@gmail.com>
2024-02-19 12:20:02 +0800

    Update atomicPath

--- vendors/atomic_design/alive/path.js
+++ vendors/atomic_design/alive/path.js
@@ -3,7 +3,10 @@ define(['app'],function(app){
 	app.register.factory("AtomicPath",[function(){
 		return {
 			url:function(file){
-				var path =require.config().toUrl("vendors/atomic_design"+file+'?rand='+Math.random());
+				var filePath = "vendors/atomic_design"+file+'?';
+					if(window.location.hostname=="localhost")	
+						filePath += 'rand='+Math.random();
+				var path =require.config().toUrl(filePath);
 				var parent = require.config().toUrl("").split("/");
 				if(parent.length>1)
 					path  =  parent[0]+'/'+path;
